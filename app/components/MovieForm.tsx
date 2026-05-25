80129ee2f8fe24534358115879c10b17e4a0ce8c
jesperringhog <jesper.ringhog@gmail.com>
2026-05-25 17:32:52 +0200

    implemented base movies fetch and html

---
+++ app/components/MovieForm.tsx
@@ -0,0 +1,6 @@
+export const MovieForm = () =>
+    <form className="w-full flex justify-center gap-10">
+      <label htmlFor="movieInput"></label>
+      <input type="text" placeholder="Movie" />
+      <button>Search</button>
+    </form>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import { hostname } from "os";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "m.media-amazon.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
